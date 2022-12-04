abcefda86c1a382228d2f8c6e78b181786787f11
Rogerio Chaves <rogeriocfj@gmail.com>
2022-12-04 23:48:39 +0100

    Fix not allowing me to type V or P by skipping shortcuts if inside text box or area

--- src/views/DesignEditor/shortcuts.tsx
+++ src/views/DesignEditor/shortcuts.tsx
@@ -8,79 +8,85 @@ const Shortcuts = () => {
   const editor = useEditor()
   const setActiveTool = useSetRecoilState(activeToolState)
 
-  const handleShortcuts = useCallback((event: KeyboardEvent) => {
-    if (!editor) return
-
-    const shortcuts = [
-      {
-        keys: ["CMD", "Z"],
-        action: editor.history.undo,
-      },
-      {
-        keys: ["CMD", "SHIFT", "Z"],
-        action: editor.history.redo,
-      },
-      {
-        keys: ["CTRL", "R"],
-        isMac: true,
-        action: editor.history.redo,
-      },
-      {
-        keys: ["CTRL", "Y"],
-        isMac: false,
-        action: editor.history.redo,
-      },
-      {
-        keys: ["V"],
-        action: () => setActiveTool(ToolType.MOVE),
-      },
-      {
-        keys: ["P"],
-        action: () => setActiveTool(ToolType.DRAWING),
-      },
-    ]
-
-    for (const shortcut of shortcuts) {
-      if (shortcut.isMac !== undefined && !!shortcut.isMac !== isMac) continue
-
-      const combination = {
-        meta: false,
-        ctrl: false,
-        shift: false,
-        key: "",
+  const handleShortcuts = useCallback(
+    (event: KeyboardEvent) => {
+      if (!editor) return
+      if ((event.target as HTMLElement).tagName == "INPUT" || (event.target as HTMLElement).tagName == "TEXTAREA") {
+        return
       }
-      for (const key of shortcut.keys) {
-        switch (key) {
-          case "CMD":
-            combination.meta = true
-            break
-          case "CTRL":
-            combination.ctrl = true
-            break
-          case "SHIFT":
-            combination.shift = true
-            break
-          default:
-            combination.key = key
-            break
+
+      const shortcuts = [
+        {
+          keys: ["CMD", "Z"],
+          action: editor.history.undo,
+        },
+        {
+          keys: ["CMD", "SHIFT", "Z"],
+          action: editor.history.redo,
+        },
+        {
+          keys: ["CTRL", "R"],
+          isMac: true,
+          action: editor.history.redo,
+        },
+        {
+          keys: ["CTRL", "Y"],
+          isMac: false,
+          action: editor.history.redo,
+        },
+        {
+          keys: ["V"],
+          action: () => setActiveTool(ToolType.MOVE),
+        },
+        {
+          keys: ["P"],
+          action: () => setActiveTool(ToolType.DRAWING),
+        },
+      ]
+
+      for (const shortcut of shortcuts) {
+        if (shortcut.isMac !== undefined && !!shortcut.isMac !== isMac) continue
+
+        const combination = {
+          meta: false,
+          ctrl: false,
+          shift: false,
+          key: "",
+        }
+        for (const key of shortcut.keys) {
+          switch (key) {
+            case "CMD":
+              combination.meta = true
+              break
+            case "CTRL":
+              combination.ctrl = true
+              break
+            case "SHIFT":
+              combination.shift = true
+              break
+            default:
+              combination.key = key
+              break
+          }
+        }
+
+        const eventMetaKey = isMac ? event.metaKey : event.ctrlKey
+        const trigger =
+          eventMetaKey == combination.meta &&
+          event.ctrlKey == combination.ctrl &&
+          event.shiftKey == combination.shift &&
+          event.key.toUpperCase() == combination.key
+
+        if (trigger) {
+          shortcut.action()
+          event.preventDefault()
+          event.stopImmediatePropagation()
+          break
         }
       }
-
-      const eventMetaKey = isMac ? event.metaKey : event.ctrlKey
-      const trigger =
-        eventMetaKey == combination.meta &&
-        event.ctrlKey == combination.ctrl &&
-        event.shiftKey == combination.shift &&
-        event.key.toUpperCase() == combination.key
-
-      if (trigger) {
-        shortcut.action()
-        event.preventDefault()
-        event.stopImmediatePropagation()
-        break
-      }
-    }
-  }, [editor, isMac, setActiveTool])
+    },
+    [editor, isMac, setActiveTool]
+  )
 
   useEffect(() => {
     if (!editor) return
