7dcb6788077fd5207bad38abab5e9bdab43cc55f
Rogerio Chaves <rogeriocfj@gmail.com>
2022-12-04 23:48:33 +0100

    Remove unused states from useAppContext, to favor using recoil hooks directly

--- src/hooks/useAppContext.tsx
+++ src/hooks/useAppContext.tsx
@@ -1,38 +1,22 @@
 import {
   activePanelState,
   activeSubMenuState,
-  currentTemplateState,
   isMobileState,
-  shapesState,
-  templatesState,
-  uploadsState,
 } from "../state/appContext"
 import { useRecoilState } from "recoil"
 
 const useAppContext = () => {
   const [isMobile, setIsMobile] = useRecoilState(isMobileState)
-  const [templates, setTemplates] = useRecoilState(templatesState)
-  const [uploads, setUploads] = useRecoilState(uploadsState)
-  const [shapes, setShapes] = useRecoilState(shapesState)
   const [activePanel, setActivePanel] = useRecoilState(activePanelState)
   const [activeSubMenu, setActiveSubMenu] = useRecoilState(activeSubMenuState)
-  const [currentTemplate, setCurrentTemplate] = useRecoilState(currentTemplateState)
 
   return {
     isMobile,
     setIsMobile,
-    templates,
-    setTemplates,
-    uploads,
-    setUploads,
-    shapes,
-    setShapes,
     activePanel,
     setActivePanel,
     activeSubMenu,
     setActiveSubMenu,
-    currentTemplate,
-    setCurrentTemplate,
   }
 }
 
