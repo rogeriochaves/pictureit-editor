d9d67aa296784f3ec8f6157e1e0524bf5af9ff14
Rogerio Chaves <rogeriocfj@gmail.com>
2022-12-04 23:48:34 +0100

    Remove unused app context state

--- src/state/appContext.ts
+++ src/state/appContext.ts
@@ -1,25 +1,9 @@
-import { Template } from "@layerhub-io/core"
 import { atom, RecoilState } from "recoil"
 import { PanelType } from "../constants/app-options"
 
 export const isMobileState: RecoilState<boolean | undefined> = atom({
   key: "isMobileState",
   default: undefined as boolean | undefined
-})
-
-export const templatesState: RecoilState<Template[]> = atom({
-  key: "templatesState",
-  default: [] as Template[],
-})
-
-export const uploadsState: RecoilState<any[]> = atom({
-  key: "uploadsState",
-  default: [] as any[],
-})
-
-export const shapesState: RecoilState<Template[]> = atom({
-  key: "shapesState",
-  default: [] as Template[],
 })
 
 export const activePanelState: RecoilState<PanelType> = atom({
@@ -30,9 +14,4 @@ export const activePanelState: RecoilState<PanelType> = atom({
 export const activeSubMenuState: RecoilState<string | null> = atom({
   key: "activeSubMenuState",
   default: null as string | null,
-})
-
-export const currentTemplateState: RecoilState<any> = atom({
-  key: "currentTemplateState",
-  default: null,
 })
